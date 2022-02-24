7b5bc177bfe236686744dd4933724f6ac3bbef2e
Chenjie <chenjieh@ucm.es>
2022-02-24 20:47:11 +0100

    failed test in comsumidor

--- 01.py
+++ 01.py
@@ -1,4 +1,4 @@
-from multiproccesing import Process
+from multiprocessing import Process, Array, BoundedSemaphore, Semaphore
 from random import randint
 
 #constantes
@@ -8,35 +8,84 @@ Mprod = 10
 
 def minIndex(storage):
 	m = 0
-	for i in range(1, Nprod):
+	for i in range(1, len(storage)):
 		if storage[i] >= 0 and (storage[m] > storage[i]):
 			m = i
 	return m
 
+def endMerge(storage):
+    n = 0
+    f = True
+    while n < len(storage) and f:
+        f = storage[n] == -1
+        n = n+1
+    return f
+    
 def productor(storage, pid, empty, nonEmpty):
 	for i in range(Mprod):
 		d = randint(0,Crec)
+        
 		empty.acquire()
+        
 		print(f"Productor {pid} produciendo...")
 		storage[pid] = storage[pid] + d
+        
 		nonEmpty.release
 		print(f"Productor {pid} almacenado...")
+        
 	empty.adquire()
 	storage[pid] = -1
 	nonEmpty.release
 	print(f"Productor {pid} terminado.")
 
 def consumidor(storage, emptys, nonEmptys, mergedList):
-	nonEnd = True
-	while nonEnd:
-		for i in range(Nproc):
-			nonEmptys[i].acquire()
-		print(f"Consumiendo...")
-		mindex = minIndex(storage)
-		mergedList.append(storage[mindex])
-		emptys[mindex].release()
-		print(f"Comsumido producto {mindex}")
+    end = False
+    while not end:
+        for i in range(Nproc):
+            nonEmptys[i].acquire()
+        print(f"Consumiendo...")
+        mindex = minIndex(storage)
+        mergedList.append(storage[mindex])
+        end = endMerge(storage)
+        emptys[mindex].release()
+        print(f"Consumido producto {mindex}")
+        
+
+def main():
+    mergedList = []
+    storage = Array('i', [0]*Nproc)
+    listEmpty = []
+    listNonEmpty = []
+    for i in range(Nproc):
+        listEmpty.append(BoundedSemaphore(1))
+        listNonEmpty.append(Semaphore(0))
+    
+    listProd = []
+    for i in range(Nproc):
+        listProd.append(Process(target=productor, 
+                                args=(storage, i, listEmpty[i], listNonEmpty[i])))
+        
+    listCons = [Process(target=consumidor,
+                        args = (storage, listEmpty, listNonEmpty, mergedList))]
+    
+    for p in listProd + listCons:
+        p.start()
+    
+    for p in listProd + listCons:
+        p.join()
+        
+    print(mergedList)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
 		
-		for i in range(Nproc):
-			nonEmptys[i].acquire()
-		nonEnd
+		
